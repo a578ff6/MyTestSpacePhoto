15f57dc1eccc2f20aa160d53ca076da52d03074b
wayne tsao <a578ff6@gmail.com>
2023-10-31 17:11:12 +0800

    Initial Commit

---
+++ MyTestSpacePhoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTestSpacePhoto
+//
+//  Created by 曹家瑋 on 2023/10/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
